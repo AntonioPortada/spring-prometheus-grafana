984c8e3e9654f3c92e180fb048bdb15ab437fc13
AntonioPortada <antonio_portada25@hotmail.com>
2026-01-23 21:34:02 -0600

    Agregando Endpoints para pruebas y configurando prometheus desde application.yaml

---
+++ src/main/java/com/tyj/prometheus_grafana/controller/HomeController.java
@@ -0,0 +1,23 @@
+package com.tyj.prometheus_grafana.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/home")
+public class HomeController {
+
+    @GetMapping({"", "/"})
+    public ResponseEntity<String> home() {
+        return ResponseEntity.ok("Home Page");
+    }
+
+    @GetMapping("/greet/{name}")
+    public ResponseEntity<String> greet(@PathVariable("name") String name) {
+        return ResponseEntity.ok("Hello from Spring with Prometheus and Grafana, " + name + "!");
+    }
+
+}
