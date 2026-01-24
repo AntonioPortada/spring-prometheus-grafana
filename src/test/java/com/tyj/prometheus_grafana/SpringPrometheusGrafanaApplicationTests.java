0afc2e68e05a114c3efb250988cae0e6a22fb32f
AntonioPortada <antonio_portada25@hotmail.com>
2026-01-23 21:15:53 -0600

    carga de proyecto

---
+++ src/test/java/com/tyj/prometheus_grafana/SpringPrometheusGrafanaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tyj.prometheus_grafana;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPrometheusGrafanaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
